d1bb4e5f0719ad505a7fd8df4948e455fd4de7d7
heroomar <107208599+heroomar@users.noreply.github.com>
2024-12-12 21:04:38 +0500

    12122024

--- fyp.sql
+++ fyp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2024 at 02:19 PM
+-- Generation Time: Dec 12, 2024 at 05:04 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -40,6 +40,35 @@ CREATE TABLE `failed_jobs` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `main_categories`
+--
+
+CREATE TABLE `main_categories` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `slug` varchar(255) NOT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `image_path` varchar(255) DEFAULT NULL,
+  `icon_path` varchar(255) DEFAULT NULL,
+  `trending` int(11) NOT NULL DEFAULT 0 COMMENT '0 => no, 1 => yes',
+  `status` int(11) NOT NULL DEFAULT 0 COMMENT '0 => Inactive, 1 => Active',
+  `theme_id` varchar(255) DEFAULT NULL,
+  `store_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `main_categories`
+--
+
+INSERT INTO `main_categories` (`id`, `name`, `slug`, `image_url`, `image_path`, `icon_path`, `trending`, `status`, `theme_id`, `store_id`, `created_at`, `updated_at`) VALUES
+(1, 'cat1', 'collections/cat1', 'http://localhost:8000/themes/grocery/uploads/80_1733298250_Capture.PNG', 'themes/grocery/uploads/80_1733298250_Capture.PNG', '/storage/uploads/default.jpg', 0, 0, 'grocery', 1, '2024-12-04 02:14:34', '2024-12-04 02:44:10'),
+(2, 'f1111', 'collections/f1111', 'http://localhost:8000/themes/grocery/uploads/98_1733298147_Capture.PNG', 'themes/grocery/uploads/98_1733298147_Capture.PNG', '/storage/uploads/default.jpg', 0, 0, 'grocery', 1, '2024-12-04 02:42:27', '2024-12-11 09:43:57');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -57,7 +86,10 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2023_12_25_093024_create_main_categories_table', 2),
+(6, '2023_12_25_094412_create_sub_categories_table', 2),
+(7, '2023_12_26_092822_create_products_table', 2);
 
 -- --------------------------------------------------------
 
@@ -93,6 +125,71 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `maincategory_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `subcategory_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT 1 COMMENT '0 => Inactive, 1 => Active',
+  `price` double(8,2) NOT NULL DEFAULT 0.00,
+  `sale_price` double(8,2) NOT NULL DEFAULT 0.00,
+  `product_stock` int(11) NOT NULL DEFAULT 0,
+  `product_weight` int(11) DEFAULT NULL,
+  `cover_image_path` varchar(255) DEFAULT NULL,
+  `cover_image_url` varchar(255) DEFAULT NULL,
+  `stock_status` varchar(255) NOT NULL DEFAULT '0',
+  `description` longtext DEFAULT NULL,
+  `detail` text DEFAULT NULL,
+  `specification` text DEFAULT NULL,
+  `theme_id` varchar(255) DEFAULT NULL,
+  `store_id` int(11) NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `is_active` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `slug`, `maincategory_id`, `subcategory_id`, `status`, `price`, `sale_price`, `product_stock`, `product_weight`, `cover_image_path`, `cover_image_url`, `stock_status`, `description`, `detail`, `specification`, `theme_id`, `store_id`, `created_by`, `is_active`, `created_at`, `updated_at`) VALUES
+(1, 'flour', 'flour', 1, 1, 1, 773.00, 15.00, 0, 543, 'themes/grocery/uploads/83_1734018411_Wheat-Flour-2-1.jpg', 'http://localhost:8000/themes/grocery/uploads/83_1734018411_Wheat-Flour-2-1.jpg', '0', NULL, NULL, NULL, 'grocery', 1, 2, NULL, '2024-12-12 10:46:51', '2024-12-12 10:46:51');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub_categories`
+--
+
+CREATE TABLE `sub_categories` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `image_path` varchar(255) DEFAULT NULL,
+  `icon_path` varchar(255) DEFAULT NULL,
+  `maincategory_id` bigint(20) UNSIGNED NOT NULL DEFAULT 0,
+  `status` int(11) NOT NULL DEFAULT 0 COMMENT '0 => Inactive, 1 => Active',
+  `theme_id` varchar(255) DEFAULT NULL,
+  `store_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sub_categories`
+--
+
+INSERT INTO `sub_categories` (`id`, `name`, `image_url`, `image_path`, `icon_path`, `maincategory_id`, `status`, `theme_id`, `store_id`, `created_at`, `updated_at`) VALUES
+(1, 'sub cat', 'http://localhost:8000/themes/grocery/uploads/76_1733929039_deposit-receipt_x.png', 'themes/grocery/uploads/76_1733929039_deposit-receipt_x.png', '/storage/uploads/default.jpg', 1, 1, 'grocery', 1, '2024-12-11 09:57:19', '2024-12-11 09:57:19');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -112,7 +209,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Daniel Daniels', 'cohutegic@mailinator.com', NULL, '$2y$12$Y9rvOUsoI3Lv71C2ksQG/e7CzjTkN7IYdvYO0YdEv0RkBW7p6MfCO', NULL, '2024-11-27 08:12:20', '2024-11-27 08:12:20');
+(1, 'Daniel Daniels', 'cohutegic@mailinator.com', NULL, '$2y$12$Y9rvOUsoI3Lv71C2ksQG/e7CzjTkN7IYdvYO0YdEv0RkBW7p6MfCO', NULL, '2024-11-27 08:12:20', '2024-11-27 08:12:20'),
+(2, 'umer', 'superadmin@gmail.com', NULL, '$2y$12$GMxQdxSYrVFP4lTAFqvoiegZOwHMk6aqmJt5qO1r2aEc6.r3zmwa2', NULL, '2024-12-03 08:30:34', '2024-12-03 08:30:34');
 
 --
 -- Indexes for dumped tables
@@ -124,6 +222,12 @@ INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `re
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `main_categories`
+--
+ALTER TABLE `main_categories`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `migrations`
@@ -146,6 +250,21 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `products_maincategory_id_foreign` (`maincategory_id`),
+  ADD KEY `products_subcategory_id_foreign` (`subcategory_id`);
+
+--
+-- Indexes for table `sub_categories`
+--
+ALTER TABLE `sub_categories`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sub_categories_maincategory_id_foreign` (`maincategory_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -163,10 +282,16 @@ ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `main_categories`
+--
+ALTER TABLE `main_categories`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -175,10 +300,39 @@ ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `sub_categories`
+--
+ALTER TABLE `sub_categories`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_maincategory_id_foreign` FOREIGN KEY (`maincategory_id`) REFERENCES `main_categories` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `products_subcategory_id_foreign` FOREIGN KEY (`subcategory_id`) REFERENCES `sub_categories` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `sub_categories`
+--
+ALTER TABLE `sub_categories`
+  ADD CONSTRAINT `sub_categories_maincategory_id_foreign` FOREIGN KEY (`maincategory_id`) REFERENCES `main_categories` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
